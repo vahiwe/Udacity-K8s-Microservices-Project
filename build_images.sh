a42f70086ee79fdd1340eaa139e98c57c91dddd9
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-06-05 23:44:06 +0100

    Update AWS Configuration

--- build_images.sh
+++ build_images.sh
@@ -9,6 +9,8 @@ docker build -t vahiwe/udagram-api-feeds \
 --build-arg AWS_BUCKET_ARG=${AWS_BUCKET} \
 --build-arg AWS_REGION_ARG=${AWS_REGION} \
 --build-arg AWS_PROFILE_ARG=${AWS_PROFILE} \
+--build-arg AWS_ACCESS_KEY_ID_ARG=${AWS_ACCESS_KEY_ID} \
+--build-arg AWS_SECRET_ACCESS_KEY_ARG=${AWS_SECRET_ACCESS_KEY} \
 --build-arg URL_ARG=${URL} -f ./microservices/api/feeds/Dockerfile ./microservices/api/feeds/
 
 docker build -t vahiwe/udagram-api-users \
@@ -20,17 +22,10 @@ docker build -t vahiwe/udagram-api-users \
 --build-arg AWS_BUCKET_ARG=${AWS_BUCKET} \
 --build-arg AWS_REGION_ARG=${AWS_REGION} \
 --build-arg AWS_PROFILE_ARG=${AWS_PROFILE} \
+--build-arg AWS_ACCESS_KEY_ID_ARG=${AWS_ACCESS_KEY_ID} \
+--build-arg AWS_SECRET_ACCESS_KEY_ARG=${AWS_SECRET_ACCESS_KEY} \
 --build-arg URL_ARG=${URL} -f ./microservices/api/users/Dockerfile ./microservices/api/users/
 
-docker build -t vahiwe/udagram-frontend \
---build-arg POSTGRES_USERNAME_ARG=${POSTGRES_USERNAME} \
---build-arg POSTGRES_PASSWORD_ARG=${POSTGRES_PASSWORD} \
---build-arg POSTGRES_HOST_ARG=${POSTGRES_HOST} \
---build-arg POSTGRES_DB_ARG=${POSTGRES_DB} \
---build-arg JWT_SECRET_ARG=${JWT_SECRET} \
---build-arg AWS_BUCKET_ARG=${AWS_BUCKET} \
---build-arg AWS_REGION_ARG=${AWS_REGION} \
---build-arg AWS_PROFILE_ARG=${AWS_PROFILE} \
---build-arg URL_ARG=${URL} -f ./microservices/frontend/Dockerfile ./microservices/frontend/
+docker build -t vahiwe/udagram-frontend -f ./microservices/frontend/Dockerfile ./microservices/frontend/
 
 docker build -t vahiwe/simple-reverse-proxy -f ./microservices/reverse-proxy/Dockerfile ./microservices/reverse-proxy/
